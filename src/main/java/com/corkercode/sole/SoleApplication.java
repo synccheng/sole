c045adc01d828a9bc00f0b95b769462a7db31d90
zoluo <luochengga@outlook.com>
2019-03-04 16:56:37 +0800

    first commit

---
+++ src/main/java/com/corkercode/sole/SoleApplication.java
@@ -0,0 +1,13 @@
+package com.corkercode.sole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/corkercode/sole/SoleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.corkercode.sole;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SoleApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
